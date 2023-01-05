6b4868a2c2ef5cdc8f4b49ef0140a626d15bd520
Martin Tan <martinwdtan@gmail.com>
2023-01-06 01:42:14 +0800

    Set up Docker with NestJS, MongoDB
    Changes:
     - Update README with instructions to start application with Docker Compose
     - Update package.json with mongoose and other dependencies
     - Add environment variables support
     - Add constants.ts to hold all environment variables in project

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+
+// Load the correct environment variables file
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+export const PORT = process.env.PORT;
+export const MONGODB_URL = process.env.MONGODB_URL;
+export const OPEN_WEATHER_MAP_API_KEY = process.env.OPEN_WEATHER_MAP_API_KEY;
+export const OPEN_WEATHER_MAP_API_BASE_URL =
+  process.env.OPEN_WEATHER_MAP_API_BASE_URL;
+export const INTERNAL_SERVER_ERROR_MESSAGE =
+  'Something went wrong, please try again.';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('Starting application on port:', PORT);
+  await app.listen(PORT);
 }
 bootstrap();
